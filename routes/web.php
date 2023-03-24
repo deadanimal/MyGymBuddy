c7d1f939e16e50584480ea966eb92faaa17cc2e5
Afeez Aziz <afeezaziz@gmail.com>
2023-03-24 22:36:13 +0800

    123

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,6 @@ Route::get('/exercises/{exercise_id}', [ExerciseController::class, 'show_exercis
 Route::put('/exercises/{exercise_id}', [ExerciseController::class, 'update_exercise']);
 
 Route::get('/exercise-attempts', [ExerciseController::class, 'show_exercise_attempts']);
-Route::post('/exercise-attempts/', [ExerciseController::class, 'create_exercise_attempts']);
+Route::post('/exercise-attempts/', [ExerciseController::class, 'create_exercise_attempt']);
 Route::get('/exercise-attempts/{exercise_attempt_id}', [ExerciseController::class, 'show_exercise_attempt']);
 Route::put('/exercise-attempts/{exercise_attempt_id}', [ExerciseController::class, 'update_exercise_attempt']);
