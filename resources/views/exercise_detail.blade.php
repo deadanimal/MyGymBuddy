4d4c747cfe56e72f4990a09cbe0055c92aaa164f
Afeez Aziz <afeezaziz@gmail.com>
2023-03-24 22:41:14 +0800

    3

--- resources/views/exercise_detail.blade.php
+++ resources/views/exercise_detail.blade.php
@@ -35,7 +35,7 @@
 <form action="/exercise-attempts/" method="POST">
     @csrf
     <input type="hidden" name="exercise_id" value="{{$exercise->id}}">
-    <input type="number" class="form-control" name="weight" placeholder="weight">
+    <input type="number" step="0.1" class="form-control" name="weight" placeholder="weight">
     <input type="number" class="form-control" name="repetition" placeholder="reps">
     <input type="text" class="form-control" name="remarks" placeholder="remarks">
     <button type="submit" class="btn btn-primary">Submit</button>
