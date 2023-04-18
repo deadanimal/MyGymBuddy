56b30d8867c1a55a2697bf795f1ab732af6baeb4
Afeez Aziz <afeezaziz@gmail.com>
2023-04-19 02:51:21 +0800

    123

--- resources/views/exercise/detail.blade.php
+++ resources/views/exercise/detail.blade.php
@@ -3,15 +3,73 @@
 @section('content')
     <header class="py-10 ">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-3">
-            <h1 class="text-3xl font-bold tracking-tight">Dashboard</h1>
+            <h1 class="text-3xl font-bold tracking-tight">{{ $exercise->name }}</h1>
         </div>
     </header>
 
 
     <main>
-        <a href="/exercises">Exercises</a>
+        <div class="px-4 sm:px-6 lg:px-8">
+            <div class="sm:flex sm:items-center">
+                <div class="sm:flex-auto">
+                    <h1 class="text-base font-semibold leading-6 text-gray-900">Users</h1>
+                    <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name,
+                        title, email and role.</p>
+                </div>
+                <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                    <button type="button"
+                        class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add
+                        user</button>
+                </div>
+            </div>
+            <div class="mt-8 flow-root">
+                <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                        <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-300">
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col"
+                                            class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                                            No</th>
+                                        <th scope="col"
+                                            class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Name</th>
+                                        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                            <span class="sr-only">View</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody class="divide-y divide-gray-200 bg-white">
+                                    @forelse($exercise->attempts as $attempt)
+                                        <tr>
+                                            <td
+                                                class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                                                {{ $loop->iteration }}
+                                            </td>
+                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                {{ $attempt->name }}
+                                            </td>
+                                            <td
+                                                class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                                <a href="/exercises/{{ $attempt->id }}"
+                                                    class="text-indigo-600 hover:text-indigo-900">
+                                                    View
+                                                    <span class="sr-only">, {{ $attempt->name }}</span>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                    @endforelse
 
-        <h1>{{ $exercise->name }}</h1>
+                                    <!-- More people... -->
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <table class="table">
             <thead>
                 <tr>
--- resources/views/exercise/list.blade.php
+++ resources/views/exercise/list.blade.php
@@ -2,7 +2,6 @@
 
 
 @section('content')
-
     <header class="py-10 ">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-3">
             <h1 class="text-3xl font-bold tracking-tight">List of Exercise</h1>
@@ -12,35 +11,73 @@
 
     <main>
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse($exercises as $exercise)
-                    <tr>
-                        <th scope="row">{{ $loop->iteration }}</th>
-                        <td><a href="/exercises/{{ $exercise->id }}">{{ $exercise->name }}</a></td>
-                    </tr>
-                @empty
-                    <tr>
-                        <th scope="row">-</th>
-                        <td>-</td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
+        <div class="px-4 sm:px-6 lg:px-8">
+            <div class="sm:flex sm:items-center">
+                <div class="sm:flex-auto">
+                    <h1 class="text-base font-semibold leading-6 text-gray-900">Users</h1>
+                    <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name,
+                        title, email and role.</p>
+                </div>
+                <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                    <button type="button"
+                        class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add
+                        user</button>
+                </div>
+            </div>
+            <div class="mt-8 flow-root">
+                <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                        <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-300">
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col"
+                                            class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                                            No</th>
+                                        <th scope="col"
+                                            class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Name</th>
+                                        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                            <span class="sr-only">View</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody class="divide-y divide-gray-200 bg-white">
+                                    @forelse($exercises as $exercise)
+                                    <tr>
+                                        <td
+                                            class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                                            {{ $loop->iteration }}
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                            {{ $exercise->name }}
+                                        </td>
+                                        <td
+                                            class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                            <a href="/exercises/{{ $exercise->id }}" class="text-indigo-600 hover:text-indigo-900">
+                                                View
+                                                <span class="sr-only">, {{ $exercise->name }}</span>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    @empty
+                                    @endforelse
 
+                                    <!-- More people... -->
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+        
 
         <form action="/exercises" method="POST">
             @csrf
             <input type="text" class="form-control" name="name">
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
-    </main>
 
+    </main>
 @endsection
