12f594249e43351d87488c365d86d39d8129fea1
Afeez Aziz <afeezaziz@gmail.com>
2023-04-18 19:00:47 +0800

    123

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+    <header class="py-10 ">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-3">
+            <h1 class="text-3xl font-bold tracking-tight">Dashboard</h1>
+        </div>
+    </header>
 
-<h1>Dashboard</h1>
-<a href="/exercises">Exercises</a>
 
-
+    <main class="-mt-32">
+    </main>
 @endsection
--- resources/views/exercise_list.blade.php
+++ resources/views/exercise_list.blade.php
@@ -2,35 +2,48 @@
 
 @section('content')
 
+    @extends('layouts.app')
 
-<table class="table">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Name</th>
-      </tr>
-    </thead>
-    <tbody>
-        @forelse($exercises as $exercise)
-      <tr>
-        <th scope="row">{{$loop->iteration}}</th>
-        <td><a href="/exercises/{{$exercise->id}}">{{$exercise->name}}</a></td>
-      </tr>
-      @empty
-      <tr>
-        <th scope="row">-</th>
-        <td>-</td>
-      </tr> 
-      @endforelse
-    </tbody>
-  </table>
+@section('content')
+
+    <header class="py-10 ">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-3">
+            <h1 class="text-3xl font-bold tracking-tight">List of Exercise</h1>
+        </div>
+    </header>
 
 
+    <main class="-mt-32">
 
-<form action="/exercises" method="POST">
-    @csrf
-    <input type="text" class="form-control" name="name">
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse($exercises as $exercise)
+                    <tr>
+                        <th scope="row">{{ $loop->iteration }}</th>
+                        <td><a href="/exercises/{{ $exercise->id }}">{{ $exercise->name }}</a></td>
+                    </tr>
+                @empty
+                    <tr>
+                        <th scope="row">-</th>
+                        <td>-</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+
+
+
+        <form action="/exercises" method="POST">
+            @csrf
+            <input type="text" class="form-control" name="name">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </main>
 
 @endsection
